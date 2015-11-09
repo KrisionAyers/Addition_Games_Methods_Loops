d2e283b5aee0b11ed1ddf976b8afc1ba2fb13927
KrisionAyers <ayersk@student.swosu.edu>
2015-11-09 09:30:49 -0600

    Adding files

---
+++ HelloWorld/src/IfThisThen.java
@@ -0,0 +1,14 @@
+
+public class IfThisThen  {
+	public static void main(String[] args) {
+		
+		boolean pencolorIsGreen = false;
+		boolean penColorIsRed   = true;
+		
+		if(penColorIsGreen){
+			System.out.println("Pen is green.");
+		}else if(penColorIsRed){	
+			System.out.println("Pen is red.")}
+		
+		
+		
---
+++ Listing_4_16_Method_Test/src/Listing_4_16_Method_Test.java
@@ -0,0 +1,7 @@
+public class Listing_4_16_Method_Test {
+	public static void main(String[] args) {
+		System.out.print("Howdy");
+
+	}
+
+}
